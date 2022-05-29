0da497a767d5eb0f39cc17e5a793d1a157386799
Gabrielforest <gabrielfreitaspereira@estudante.ufscar.br>
2022-05-29 20:15:34 -0300

    problem 16 in Julia

---
+++ problems/problem_16.jl
@@ -0,0 +1,8 @@
+
+# Problem 16 --------------------------------------------------------------
+
+# 215 = 32768 and the sum of its digits is 3 + 2 + 7 + 6 + 8 = 26.
+
+# What is the sum of the digits of the number 21000?
+
+sum( collect( digits( BigInt( 2 )^1000 ) ) )
